5fabd8bf77af07c1e40a14fc5f3f00286e98803c
les.ruiz@gmail.com <manath4gh>
2017-08-20 14:21:10 +0200

    Update

--- openrevman/control_computer/test/test_solve_it.py
+++ openrevman/control_computer/test/test_solve_it.py
@@ -6,6 +6,8 @@ from openrevman.control_computer import solver
 
 class TestSolve_it(TestCase):
     def test_solve_it(self):
-        c = StringIO("0 1\n2 3")
-        d = StringIO("0 1")
-        solver.solve_it(demand_data=c, capacity_data=d)
+        d = StringIO("5 2")
+        p = StringIO("1 2")
+        c = StringIO("1 1")
+        dud = StringIO("0 1\n2 3")
+        solver.solve_it(demand_data=d, price_data=p,capacity_data=c,demand_utilization_data=dud)
