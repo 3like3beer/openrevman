be7c8de48db582b068de6679dcbbd5f21eac0389
les.ruiz@gmail.com <manath4gh>
2017-08-20 15:25:45 +0200

    Update

--- openrevman/control_computer/solver.py
+++ openrevman/control_computer/solver.py
@@ -22,7 +22,7 @@ def solve_it(demand_data, price_data, capacity_data, demand_utilization_data):
     demand_utilization_matrix  = loadtxt(demand_utilization_data)
 
     # run optimization algorithm
-    value = pulp_solve(demand_vector, price_vector, capacity_vector,demand_utilization_matrix)
+    value = pulp_solve(demand_vector,capacity_vector, price_vector, demand_utilization_matrix)
 
     # prepare the solution in the specified output format
     output_data = str(value) + ' ' + str(0) + '\n'
@@ -33,14 +33,19 @@ def pulp_solve(demand_vector, capacity_vector, price_vector,demand_utilization_m
     revman = pulp.LpProblem("revman", pulp.LpMaximize)
     x = [pulp.LpVariable(name="x" + str(i),lowBound= 0,cat= pulp.LpContinuous) for (i,t) in enumerate(demand_vector)]
 
-    objective = pulp.LpAffineExpression([(x[i.index], price_vector[i.index]) for i in demand_vector])
+    objective = pulp.LpAffineExpression([(x[i], price_vector[i]) for (i,d) in enumerate(demand_vector)])
     revman.setObjective(objective)
-
-    revman += sum([i * x[i.index] for i in demand_utilization_matrix]) <= capacity_vector
-    revman += ([x[i] for (i,t) in enumerate(demand_vector)]) <= demand_vector
+    #pulp.LpConstraint
+    #a = [x[i] * d for (i,d) in enumerate(demand_utilization_matrix)]
+    #print ([x[i] * d for (i,d) in enumerate(demand_utilization_matrix)])
+    for (product_index,capacity) in enumerate(capacity_vector):
+        revman += pulp.lpSum([x[i] * demand_utilization_matrix[i,product_index] for (i,d) in enumerate(demand_vector)]) <= capacity
+    for (i,demand) in enumerate(demand_vector):
+        revman += (x[i] ) <= demand
 
     revman.solve(pulp.PULP_CBC_CMD())
-
+    revman.writeLP("temp.txt")
+    print(pulp.LpStatus[revman.status])
     accepted_demand = [i.value() for i in x]
     print(accepted_demand)
     return accepted_demand
--- openrevman/control_computer/test/test_solve_it.py
+++ openrevman/control_computer/test/test_solve_it.py
@@ -7,7 +7,7 @@ from openrevman.control_computer import solver
 class TestSolve_it(TestCase):
     def test_solve_it(self):
         d = StringIO("5 2")
-        p = StringIO("1 2")
+        p = StringIO("1 20")
         c = StringIO("1 1")
         dud = StringIO("0 1\n2 3")
         solver.solve_it(demand_data=d, price_data=p,capacity_data=c,demand_utilization_data=dud)
