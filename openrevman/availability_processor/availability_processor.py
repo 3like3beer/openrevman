ab7a75dcd9c4d166b666b12fca6d8be7bde9a32a
les.ruiz@gmail.com <manath4gh>
2017-08-28 22:23:29 +0200

    Update

--- openrevman/availability_processor/availability_processor.py
+++ openrevman/availability_processor/availability_processor.py
@@ -1,11 +1,11 @@
 class AvailabilityProcessor:
-    def __init__(self, controls,product_bid_prices):
+    def __init__(self, controls, product_bid_prices):
         self.controls = controls
         self.product_bid_prices = product_bid_prices
 
-    def get_price(self,product):
-        return 0
+    def get_price(self, product):
+        return product * self.product_bid_prices
 
-    def is_available(self,demand):
+
+    def is_available(self, demand):
         return True
-
--- openrevman/inventory/inventory.py
+++ openrevman/inventory/inventory.py
@@ -1,4 +1,3 @@
 class Inventory:
     def __init__(self, remaining_capacity=None):
         self.remaining_capacity = remaining_capacity
-    
