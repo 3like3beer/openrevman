6512daaebc2b2fb962232231c274500858bad961
les.ruiz@gmail.com <manath4gh>
2017-08-29 20:35:17 +0200

    Update

---
+++ openrevman/availability_processor/test/test_availabilityProcessor.py
@@ -0,0 +1,9 @@
+from unittest import TestCase
+
+
+class TestAvailabilityProcessor(TestCase):
+    def test_get_price(self):
+        self.fail()
+
+    def test_is_available(self):
+        self.fail()
