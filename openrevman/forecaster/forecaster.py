76c9d68d3f127fd5a9e822929354c586418e9595
les.ruiz@gmail.com <manath4gh>
2017-09-05 21:28:42 +0200

    Update

---
+++ openrevman/forecaster/forecaster.py
@@ -0,0 +1,12 @@
+class Forecaster:
+    def __init__(self, remaining_capacity=None, demand_utilization_matrix=None):
+        self.parameters = []
+
+    def update_parameter(self):
+        self.parameters = 1
+
+    def fit_parameters(self):
+        self.parameters = 1
+
+    def forecast(self):
+        return 0
