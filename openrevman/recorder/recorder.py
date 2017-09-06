0d869576f762fe56ccc57caf012483c66176904c
les.ruiz@gmail.com <manath4gh>
2017-09-06 19:09:22 +0200

    Update

--- openrevman/recorder/recorder.py
+++ openrevman/recorder/recorder.py
@@ -8,3 +8,19 @@ class Recorder:
     @property
     def get_records(self):
         return self.records
+
+    @property
+    def get_bookings(self):
+        return self.filter_by_type("bookings")
+
+    def filter_by_type(self, value) -> object:
+        for record in self.records:
+            if record.type == value:
+                yield record
+
+class Record:
+    def __init__(self, type):
+        self.type = type
+
+
+
