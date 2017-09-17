c4aa3357b639a24d79625ff8f7f8ea368124a5e5
les.ruiz@gmail.com <manath4gh>
2017-09-17 16:30:32 +0200

    Update

--- openrevman/forecaster/forecaster.py
+++ openrevman/forecaster/forecaster.py
@@ -1,3 +1,6 @@
+from io import StringIO
+
+
 class Forecaster:
     def __init__(self, remaining_capacity=None, demand_utilization_matrix=None):
         self.parameters = []
@@ -9,4 +12,7 @@ class Forecaster:
         self.parameters = 1
 
     def forecast(self):
-        return 0
+        demand_data = StringIO("1 1 1")
+        demand_utilization_data = StringIO("0 1\n1 0\n1 1")
+        price_data = StringIO("10 20 20")
+        return demand_data, demand_utilization_data, price_data
--- openrevman/inventory/inventory.py
+++ openrevman/inventory/inventory.py
@@ -1,3 +1,6 @@
+from io import StringIO
+
+
 class Inventory:
     def __init__(self, remaining_capacity=None, products=None):
         self.product_inventory = dict(zip(products, remaining_capacity))
@@ -6,3 +9,6 @@ class Inventory:
         for booking in bookings:
             for product in booking.products:
                 self.product_inventory[product] = self.product_inventory[product] - 1
+
+    def get_capacity(self):
+        return StringIO(" ".join(str(x) for x in self.product_inventory.values()))
